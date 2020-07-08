29ca0e03e856cc34d7fe761e4fb884a28538a9cb
Christian Real <201513700@ingenieria.usac.edu.gt>
2020-07-08 10:16:54 -0600

    Initial Backend created

---
+++ server/database.js
@@ -0,0 +1,38 @@
+const mysql = require('mysql');
+
+// Database Connection for Production
+
+// let config = {
+//     user: process.env.SQL_USER,
+//     database: process.env.SQL_DATABASE,
+//     password: process.env.SQL_PASSWORD,
+// }
+
+// if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
+//   config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+// }
+
+// let connection = mysql.createConnection(config);
+
+// Database Connection for Development
+
+let connection = mysql.createConnection({
+  //host: process.env.DB_HOST,
+  host: '34.68.157.78',
+  //user: process.env.DB_USER,
+  user: 'root',
+  //password: process.env.DB_PASS,
+  password: '123456',
+  //database: process.env.DB_DATABASE
+  database: 'prueba'
+});
+
+  connection.connect(function(err) {
+    if (err) {
+      console.error('Error connecting: ' + err.stack);
+      return;
+    }
+    console.log('Connected as thread id: ' + connection.threadId);
+  });
+
+  module.exports = connection;
---
+++ server/routes/api/posts.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const connection = require('../../database');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+const router = express.Router();
+
+//GET
+router.get('/', (req, res) => {
+    connection.query('SELECT name FROM prueba', function (err, rows, fields) {
+        if (err) throw res.send('error: ' + err)
+        res.json(rows)
+        //console.log('The solution is: ', rows[0].name)
+    });
+});
+
+//ADD
+router.post('/', urlencodedParser, (req, res) => {
+    if(!req.body.name){
+        res.status(400)
+        res.json({
+            error: "Bad Data"
+        })
+    }else{
+        connection.query('INSERT INTO prueba (name) VALUES (\''+req.body.name+'\')', function (err, rows, fields) {
+            if (err) throw res.send('error: ' + err)
+            res.send("name added")
+        });
+    }
+});
+
+//DELETE
+router.delete('/:id', (req, res) => {
+    connection.query('DELETE FROM prueba WHERE id='+req.params.id, 
+    function (err, rows, fields) {
+        if (err) throw res.send('error: ' + err)
+        res.send("name deleted")
+    });
+});
+
+//UPDATE
+router.put('/:id', urlencodedParser, (req, res) => {
+    if(!req.body.name){
+        res.status(400)
+        res.json({
+            error: "Bad Data"
+        })
+    }else{
+        connection.query('UPDATE prueba SET name = \''+req.body.name+'\' WHERE id='+req.params.id, 
+        function (err, rows, fields) {
+            if (err) throw res.send('error: ' + err)
+            res.send("name updated")
+        });
+    }
+});
+
+module.exports = router;
