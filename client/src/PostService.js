3f0d5cf088a0a7a4deb4253cf6ab5b8f933de487
cristianreal <201513700@ingenieria.usac.edu.gt>
2020-07-08 23:34:07 -0600

    contenedor server additionooonnonON

--- client/src/PostService.js
+++ client/src/PostService.js
@@ -1,25 +1,25 @@
-import axios from 'axios';
+//import axios from 'axios';
 //http://127.0.0.1:5000/api/posts
-const _IP = "34.69.42.189";
-const _PORT = "5000";
-const _PATH = "/api/posts/"
+//const _IP = "34.69.42.189";
+//const _PORT = "5000";
+//const _PATH = "/api/posts/"
 
-const url = "http://"+ _IP + ":" + _PORT + _PATH
+//const url = "http://"+ _IP + ":" + _PORT + _PATH
 
 class PostService {
     //-----------------GET-----------------
 
     //-----------------CREATE-----------------
-    static insertPost(name){
-        return axios.post(url, {
-            name
-        });
-    }
+    //static insertPost(name){
+    //    return axios.post(url, {
+    //        name
+    //    });
+    //}
 
     //-----------------DELETE-----------------
-    static deletePost(id) {
-        return axios.delete(`${url}${id}`);
-    }
+    //static deletePost(id) {
+    //    return axios.delete(`${url}${id}`);
+    //}
 
 }
 
--- client/src/components/PostComponent.vue
+++ client/src/components/PostComponent.vue
@@ -24,9 +24,9 @@
 
 <script>
 import axios from 'axios';
-const _IP = "34.69.42.189";
-const _PORT = "5000";
-const _PATH = "/api/posts/"
+const _IP=process.env.API_IP;
+const _PORT=process.env.API_PORT;
+const _PATH = "/api/posts/";
 
 const url = "http://"+ _IP + ":" + _PORT + _PATH
 export default {
